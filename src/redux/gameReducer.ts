ebac37805bc2e30097eb7783d2b2d96f331bb34e
Serhii Kovalov <andrey_roland@hotmail.com>
2022-02-23 10:05:55 +0200

    update: refactor redux files to ts

--- src/redux/gameReducer.ts
+++ src/redux/gameReducer.ts
@@ -1,20 +1,20 @@
-import { Reset_Game, Set_Marks, Set_StepNumber, Set_Player } from "./types"
+import * as actionTypes  from "./types"
 
-const defaultState = {
+const defaultState: GameState = {
     marks: Array(9).fill(""),
     stepNumber: 1,
     player: 1,
 }
 
-export const gameReducer = (state = defaultState, action) => {
+export const gameReducer = (state: GameState = defaultState, action: GameAction) => {
     switch (action.type) {
-        case Reset_Game:
+        case actionTypes.Reset_Game:
             return defaultState;
-        case Set_Marks:
+        case actionTypes.Set_Marks:
             return { ...state, marks: action.payload }
-        case Set_StepNumber:
+        case actionTypes.Set_StepNumber:
             return { ...state, stepNumber: action.payload }
-        case Set_Player:
+        case actionTypes.Set_Player:
             return { ...state, player: action.payload }
         default:
             return state;
--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { gameReducer } from "./gameReducer";
 
-export const rootReducer =  combineReducers({
+export const rootReducer = combineReducers({
     game: gameReducer
 })
+
+export type RootState = ReturnType<typeof rootReducer>
